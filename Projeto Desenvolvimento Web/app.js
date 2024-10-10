a6104156db039fa0545e92b0428ee39da8c7a3a0
Guilherm021 <guilhermepereirafzd@gmail.com>
2024-10-10 19:13:21 -0300

    Projeto
    Dados base

---
+++ Projeto Desenvolvimento Web/app.js
@@ -0,0 +1,39 @@
+function pesquisar() {
+    // Obtém a seção HTML onde os resultados serão exibidos
+    let section = document.getElementById('resultados-pesquisa');
+    let campoPesquisa = document.getElementById('campo-pesquisa').value; 
+    if (campoPesquisa == ""){
+        section.innerHTML = "<p>Nada foi encontrado. Voce precisar digitar o nome de um atleta ou esporte.</p>"
+        return
+    }
+  campoPesquisa = campoPesquisa.toLowerCase()
+
+    // Inicializa uma string vazia para armazenar os resultados
+    let resultados = '';
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
+  
+    // Intera sobre cada dado da lista de dados
+    for (let dado of dados) {
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase()
+      if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+        resultados += `
+          <div class="item-resultado">
+            <h2><a href="#" target="_blank">${dado.titulo}</a></h2>
+            <p class="descricao-meta">${dado.descricao}</p>
+            <a href="${dado.link}" target="_blank">Mais informações</a>
+          </div>
+
+        `;
+      }
+    }
+    if (!resultados){
+        resultados = "<p> Nada foi Encontrado</p"
+    }
+  
+    // Atribui os resultados gerados à seção HTML
+    section.innerHTML = resultados;
+  }
---
+++ Projeto Desenvolvimento Web/dados.js
@@ -0,0 +1,20 @@
+let dados = [
+  {
+    titulo: "Rayssa Leal",
+    descricao: " Rayssa Leal , a Fadinha do skate, é a mais jovem medalhista olímpica brasileira de todos os tempos. Com seu talento e carisma, ela inspira milhões de jovens",
+    link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+    tags: "skate fada sk8"
+  },
+  {
+    titulo: "Beatriz Souza",
+    descricao: "Beatriz Souza é uma judoca brasileira que se destaca pela sua técnica e determinação. Com diversas medalhas em competições nacionais e internacionais, ela é uma das grandes promessas do judô brasileiro.",
+    link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+    tags : "judô judo judoka"
+  },
+  {
+    titulo: "Rebeca Andrade",
+    descricao: "Rebeca Andrade é uma das maiores ginastas brasileiras de todos os tempos. Com sua força, elegância e determinação, ela conquistou o coração dos brasileiros e se tornou um verdadeiro ícone do esporte.",
+    link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+    tags : "ginástica ginastica ouro"
+  },
+];
